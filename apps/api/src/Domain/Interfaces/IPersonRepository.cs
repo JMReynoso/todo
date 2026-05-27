36b552d8c1d6965cbb9408d42d3daea5fded7bb5
Justin Reynoso <justin.reynoso625@gmail.com>
2026-05-27 10:32:57 -0400

    added exposable interfaces

---
+++ apps/api/src/Domain/Interfaces/IPersonRepository.cs
@@ -0,0 +1,12 @@
+using api.Domain.Entities;
+
+namespace api.Domain.Interfaces;
+
+public interface IPersonRepository
+{
+    Task<Person?> GetByIdAsync(int id, CancellationToken ct = default);
+    Task<IReadOnlyList<Person>> GetAllAsync(CancellationToken ct = default);
+    Task AddAsync(Person person, CancellationToken ct = default);
+    Task<bool> RemoveAsync(int id, CancellationToken ct = default);
+    Task SaveChangesAsync(CancellationToken ct = default);
+}
--- apps/api/src/Domain/Interfaces/ITodoRepository.cs
+++ apps/api/src/Domain/Interfaces/ITodoRepository.cs
@@ -4,8 +4,9 @@ namespace api.Domain.Interfaces;
 
 public interface ITodoRepository
 {
-    Task<Todo?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<Todo?> GetByIdAsync(int id, CancellationToken ct = default);
     Task<IReadOnlyList<Todo>> GetAllAsync(CancellationToken ct = default);
     Task AddAsync(Todo todo, CancellationToken ct = default);
+    Task<bool> RemoveAsync(int id, CancellationToken ct = default);
     Task SaveChangesAsync(CancellationToken ct = default);
 }
